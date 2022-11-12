130d3d3455f272a68de6b7025083f78765613cb4
josehermosilla <jose@abaqus.cl>
2022-11-12 09:07:52 -0300

    cambios optimizacion es primo

--- carta/esprimo.py
+++ carta/esprimo.py
@@ -1,5 +1,15 @@
-def es_primo():
+def es_primo(numero):
     primo = True
     contador = 2
-    while(contador <= numero**1/2 and primo):
-        
+    while contador <= numero**1 / 2 and primo:
+        if numero % contador == 0:
+            primo = False
+        contador += 1
+
+    return False if (numero < 2) else primo
+
+
+if __name__ == "__main__":
+    for i in range(1, 1001):
+        if es_primo(i):
+            print(i)
