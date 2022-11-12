8741e89949fd7ddb13f9a61e1182f35e33f701a5
josehermosilla <jose@abaqus.cl>
2022-11-12 10:22:20 -0300

    se crea el sistema para dibujar un poligono

--- carta/tortuga.py
+++ carta/tortuga.py
@@ -1,8 +1,23 @@
+"""metodos permitidos en turtle
+forward -> fd
+backward -> bk
+right -> rt
+left ->lt 
+"""
 import turtle
 
-dibujo = turtle.Turtle()
-for i in range(10):
-    dibujo.forward(200)
-    dibujo.right(90)
 
-turtle.done()
+def dibujar_poligonos(lados=4, tamano=100):
+    dibujo = turtle.Turtle()
+    dibujo.color("black", "green")
+    dibujo.begin_fill()
+    angulo = 360.0 / lados
+    for i in range(0, lados + 1):
+        dibujo.forward(tamano)
+        dibujo.right(angulo)
+    dibujo.end_fill()  # al finalizar el dibujo se pinta
+    turtle.done()
+
+
+if __name__ == "__main__":
+    dibujar_poligonos(20, 40)
